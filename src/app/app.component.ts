79c531fca5a912e878633dfd57f3766b11344718
Tim Doppenberg <tim.doppenberg@carapaxit.nl>
2022-11-12 18:46:48 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cpx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'color-warp';
+}
