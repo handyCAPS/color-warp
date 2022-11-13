431be78d125450c32247ad6c1bea6018b1a606c2
Tim Doppenberg <tim.doppenberg@carapaxit.nl>
2022-11-13 01:31:48 +0100

    Adds hslToRgb function

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -53,7 +53,57 @@ export class ColorService {
     return 'hsl(' + h + ',' + s + '%,' + l + '%)';
   }
 
-  getHslFromOffset(color: string, offsets: ColorOffset['offsetArray'], dark?: Boolean): string {
+  hslToRgb(hsl: string): string {
+    const hslArray = hsl.replace(/[^0-9,]/g, '').split(',');
+    let h = parseInt(hslArray[0]);
+    let s = parseInt(hslArray[1]);
+    let l = parseInt(hslArray[2]);
+    s /= 100;
+    l /= 100;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+      m = l - c / 2,
+      r = 0,
+      g = 0,
+      b = 0;
+    if (0 <= h && h < 60) {
+      r = c;
+      g = x;
+      b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x;
+      g = c;
+      b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0;
+      g = c;
+      b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0;
+      g = x;
+      b = c;
+    } else if (240 <= h && h < 300) {
+      r = x;
+      g = 0;
+      b = c;
+    } else if (300 <= h && h < 360) {
+      r = c;
+      g = 0;
+      b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+
+  getHslFromOffset(
+    color: string,
+    offsets: ColorOffset['offsetArray'],
+    dark?: Boolean
+  ): string {
     const hslColor = this.hexToHSL(color);
     const colorPartials = hslColor
       .replace(/[^0-9,]/g, '')
@@ -87,9 +137,7 @@ export class ColorService {
   }
 
   getLightnessFromHsl(hslColor: string): number {
-    const colorPartials = hslColor
-      .replace(/[^0-9,]/g, '')
-      .split(',');
+    const colorPartials = hslColor.replace(/[^0-9,]/g, '').split(',');
 
     return parseInt(colorPartials[2]);
   }
